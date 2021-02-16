7cc0b3865530defce530cef7fe4ffc304448df65
PSW <sangwookp9591@gmail.com>
2021-02-16 21:50:29 +0900

    JPA DAY-5 데이터 스키마 자동 생성, 필드와 컬럼 매핑

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -10,99 +10,28 @@ public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //변수 추출하기 (Extract -> Variable) Ctrl+Alt+v
-/*        EntityManagerFactory 는  application loading 시점에 딱하나만 만들어놔야한다.
-          실제 db에 저장하는 transcation 단위 고객이 행위, 상품을 장바구니에 담을때마다 DB CONNECTION 을 얻어서 QUERY를 날리고 종료되는 한 일괄적인 단위를 할때마다 EM을 만들어줘야한다.
-*/
 
         EntityManager em = emf.createEntityManager(); //이름 일괄 변경하기 단축키 .. shift +f6 emf가 고객의 요청이 올때마다 em을 생성한다.
 
-        /*
-         *JPA에선 Transcation 단위가 중요하다. 모든 data를 변경하는 작업은 transction 안에서 해야한다.
-         * */
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
 
         try{
-//            //비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-//
-//            //영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
-//
-//            Member findMember = em.find(Member.class, 101L);
-//            //find를 했는데 왜 select 쿼리가 나오지 않는가? persist할때 값을 저장하고 1차 캐시에 저장했기 때문이다.!
-//
-//            System.out.println("findMember.id = "+findMember.getId());
-//            System.out.println("findMember.name = "+findMember.getName());
+            Member member =  new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L);
-//
-//            System.out.println("result = "+ (findMember1 == findMember2));
-//
-//            Member member1 = new Member(150l,"A");
-//            Member member2 = new Member(160l,"B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-            //영속성 컨텍스트 1차 캐시 및 쓰기 지연 sql 저장소에 차곡차곡 저장이 된다.
-            //<property name="hibernate.jdbc.batch_size" value="10"/> 를 하게 되면 , size만큼 모아서 db에 한번에 network로 query를 보낸다.
-
-            Member member = new Member(200l,"member200");
             em.persist(member);
-
-            em.flush(); //db에 쿼리 즉시 발생
-            System.out.println("=======================");
             tx.commit(); //commit시점에 query가 날라가게 된다.
         } catch (Exception e){
             tx.rollback();
         }finally {
             em.close(); // EntityManager가 내부적으로 DB CONNECTION을 물고 동작하기때문에 사용다하면 자원해제를 꼭해줘야한다.
         }
-        //code
-//        try {
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
-//            em.persist(member); //쿼리를 직접 만든적은 없지만 JPA가 Mapping 정보를 보고 직접 넣어준 것이다.
-
-            //찾기
-//            Member findMember = em.find(Member.class, 1L); //em.find (type, value)
-            //수정
-//            findMember.setName("HelloJPA");
-            //setName을 한 이후 다시 persist로 저장을 할 필요가 없다. why? 자바 collection기준으로 만들어졋기 때문이다.
-            //또한 JPA를 통해서 이렇게 Entity를 가져오면 JPA가 관리를 하게 된다. JPA가뭔가 변경이 됐는지 안됐는지 Transaction을 커밋하는 시점에 체크하고 뭔가 바껴있으면 update쿼리를 날린다.
-
-            //JPQL -> 객체를 대상으로하는 객체 지향적 쿼리 -방언에 맞춰서 각DB에 맞게 표현을 해준다.
-            //전체조회
-            // 조회 대상이 일반 쿼리와 다르게 table 기준이아니라 객체이다. Member 객체 전체를 가지고와!
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .getResultList();
-            //페이징 1번부터 ~몇개 가지고와
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(1)
-//                    .setMaxResults(10)
-//                    .getResultList();
-//            for (Member member : result) {
-//                System.out.println("member.name = "+member.getName());
-//            }
-//            tx.commit(); //트랜젝션 commit 하는 순간에 영속성 컨텍스트가 db에 날라가게 된다.
-//        } catch (Exception e){
-//            tx.rollback();
-//        }finally {
-//            em.close(); // EntityManager가 내부적으로 DB CONNECTION을 물고 동작하기때문에 사용다하면 자원해제를 꼭해줘야하낟.
-//        }
-
-        //위의 코드는 실제로는 사용할 필요가없다 . Spring framework가 다해주기때문에.
 
         emf.close(); //WebApplication일 경우 was가 내려갈때 emf를 자원해제 해줘야한다 그래야 connection pooling이랑 resource가 내부적으로 release된다.
     }
-    //영속성 컨텍스트 -> 실제 JPA가 내부에서 어떻게 동작해.
-    //비영속상태 JPA와 전혀 관계가 없는 상태
-    //em.remove 객체 삭제 실제로 db삭제를 요청한 상태
+
 }
--- src/main/java/hello/jpa/Member.java
+++ src/main/java/hello/jpa/Member.java
@@ -1,28 +1,35 @@
 package hello.jpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity // jpa가 처음 로딩될때 jpa를 사용하는 애구나 얘를 관리해야겠다고 생각함
-//@Table(name = "USER") 테이블 이름이 객체이름이랑 다를경우
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+@Entity
 public class Member {
-    @Id // PK 설정
+    @Id
     private Long id;
 
-    //@Column(name = "username") field 명이 column 이름이랑 다를경우.
-    private String name;
+    @Column(name = "name",updatable = false) //DB의 컬럼명은 NAME이야 , updatable = false로 하면 이컬럼은 절때 변경되지 않는다.
+    private String username;
 
-    //JPA는 내부적으로 reflection을 쓰기때문에 동적으로 객체를 생성해놔야한다. 그래서 기본생성자가 필요하다.
-    public Member() {
+    private Integer age;
 
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob//DB에 VARCHAR를 넘어서는 큰 컨탠츠
+    private String description;
+    //Getter, Setter…
+
 
     public Long getId() {
         return id;
@@ -32,11 +39,51 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
